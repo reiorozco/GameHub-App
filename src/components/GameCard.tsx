53a17f465276a4ee2398729f078ed82729cd0331
reiorozco <rfoc15@gmail.com>
2023-03-19 17:18:16 -0500

    Display Critic Score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,8 +1,9 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 
 import { Game } from "../hooks/useGames";
 
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -16,9 +17,13 @@ function GameCard({ game }: Props) {
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
 
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,8 +13,8 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-
   parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
