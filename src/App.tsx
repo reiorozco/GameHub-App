14fda5d0301eef682887ea974811087525626773
reiorozco <rfoc15@gmail.com>
2023-03-19 16:03:52 -0500

    Fetch the Games

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
@@ -14,13 +16,11 @@ function App() {
       </GridItem>
 
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
 
-      <GridItem area="main" bg="dodgerblue">
-        Main
+      <GridItem area="main">
+        <GameGrid />
       </GridItem>
     </Grid>
   );
---
+++ src/services/api-client.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+
+  params: {
+    key: API_KEY,
+  },
+});
