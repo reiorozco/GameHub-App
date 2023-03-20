f98bb8a1f6742d9cb29f31719d591d2acdc46e73
reiorozco <rfoc15@gmail.com>
2023-03-20 16:52:50 -0500

    Try vercel env variable

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
+// const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
+const API_KEY = import.meta.env.VITE_VERCEL_ENV_RAWG_API_KEY;
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
