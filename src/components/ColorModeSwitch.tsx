757b679656dd306c4725f44f1e00d43ce88fa681
reiorozco <rfoc15@gmail.com>
2023-03-20 15:12:42 -0500

    Build Search Input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -11,7 +11,7 @@ function ColorModeSwitch() {
         onChange={toggleColorMode}
       />
 
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,16 @@
 import { HStack, Image } from "@chakra-ui/react";
 
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 import logo from "../assets/logo.webp";
 
 function NavBar() {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} alt="logo app" boxSize="60px" />
+
+      <SearchInput />
 
       <ColorModeSwitch />
     </HStack>
