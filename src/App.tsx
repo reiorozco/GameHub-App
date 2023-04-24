6706d7e1a8eda72e1f8e8e142dbadb0ebecf6a8c
reiorozco <rfoc15@gmail.com>
2023-04-24 18:48:29 -0500

    refactor: Simplify Query Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
 
-import { Genre } from "./hooks/useGenres";
 import { Platform } from "./hooks/usePlatforms";
 
 import NavBar from "./components/NavBar";
@@ -12,8 +11,8 @@ import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
-  genre: Genre | null;
-  platform: Platform | null;
+  genreId?: number;
+  platformId?: number;
   sortOrder: string;
   searchText: string;
 }
@@ -43,8 +42,10 @@ function App() {
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
           <GenreList
-            selectedGenre={gameQuery.genre}
-            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+            selectedGenreId={gameQuery.genreId}
+            onSelectGenre={(genre) =>
+              setGameQuery({ ...gameQuery, genreId: genre.id })
+            }
           />
         </GridItem>
       </Show>
@@ -55,9 +56,9 @@ function App() {
 
           <Flex gap={5} marginBottom={4}>
             <PlatformSelector
-              selectedPlatform={gameQuery.platform}
+              selectedPlatformId={gameQuery.platformId}
               onSelectedPlatform={(platform) =>
-                setGameQuery({ ...gameQuery, platform })
+                setGameQuery({ ...gameQuery, platformId: platform.id })
               }
             />
 
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -5,11 +5,14 @@ import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   onSelectedPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform | null;
+  selectedPlatformId?: number;
 }
 
-function PlatformSelector({ onSelectedPlatform, selectedPlatform }: Props) {
+function PlatformSelector({ onSelectedPlatform, selectedPlatformId }: Props) {
   const { data, isLoading, error } = usePlatforms();
+  const selectedPlatform = data?.results.find(
+    (p) => p.id === selectedPlatformId
+  );
 
   if (error) return null;
 
